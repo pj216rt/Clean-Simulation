f35972a1f05c0a59fcb3c7071507292ada830137
pjswanson <pjswanson@fsu.edu>
2025-06-13 12:02:14 -0400

    Ran and tested the univariate Gibbs sampler from Goldsmith

--- functions.R
+++ functions.R
@@ -1,6 +1,11 @@
 library(tidyverse)
 library(splines) #for working with b splines
 library(mvtnorm) #multivariate normal
+library(spam)
+library(MASS)
+library(MCMCpack) #for the inverse wishart distribution
+require(gridExtra)
+library(bayesplot)
 
 ##univariate functional portion
 beta.gen.fun.uni <- function(grid.size, knots, parameters, deg){
@@ -255,7 +260,228 @@ gen.3d.data <- function(num.subj = 50, num.visits = 5,
 }
 
 #goldsmiths uni variate Gibbs sampler
+goldsmiths.one.d.gibbs <- function(Y, fixed_design.mat, random_design.mat, 
+                                   Kt, N.iter = 1000, N.burn = 200, alpha = .1){
+  ##This Gibbs sampler accepts a Y matrix composed of concatenated observations
+  ##for one dimension, a fixed effect design matrix, a random effect design
+  ##matrix, the number of knots to be fit for each dimension, as well as overrideable 
+  ##defaults for the number of iterations and number of burn in iterations.  One can
+  ##also change the alpha term.
+  
+  set.seed(1)
+  
+  ## fixed and random effect design matrices
+  # W.des = model.matrix( fixef.form, data = data)
+  # W.des <- W.des[rep(seq_len(nrow(W.des)), length(id)), ]
+  # Z.des = model.matrix( ~ 0 + as.factor(id) + (-1):as.factor(id))
+  # W.des = as.spam(W.des)
+  # print(dim(W.des))
+  # Z.des = as.spam(Z.des)
+  ## fixed and random effect design matrices
+  #W.des = model.matrix( fixef.form, data = data)
+  #Z.des = model.matrix( ~ 0 + as.factor(id) + (-1):as.factor(id))
+  W.des = as.spam(fixed_design.mat)
+  Z.des = as.spam(random_design.mat)
+  
+  I = dim(Z.des)[2]
+  D = dim(Y)[2]
+  Ji = as.numeric(apply(Z.des, 2, sum))
+  IJ = sum(Ji)
+  p = dim(W.des)[2]
+  
+  ## bspline basis and penalty matrix
+  Theta = bs(1:D, df=Kt, intercept=TRUE, degree=3)
+  
+  diff0 = diag(1, D, D)
+  diff2 = matrix(rep(c(1,-2,1, rep(0, D-2)), D-2)[1:((D-2)*D)], D-2, D, byrow = TRUE)
+  P0 = t(Theta) %*% t(diff0) %*% diff0 %*% Theta
+  P2 = t(Theta) %*% t(diff2) %*% diff2 %*% Theta
+  P.mat = alpha * P0 + (1-alpha) * P2
+  
+  SUBJ = factor(apply(Z.des %*% 1:dim(Z.des)[2], 1, sum))
+  
+  ## find first observation
+  firstobs = rep(NA, length(unique(SUBJ)))
+  for(i in 1:length(unique(SUBJ))){
+    firstobs[i] = which(SUBJ %in% unique(SUBJ)[i])[1]
+  }
+  Wi = W.des[firstobs,]
+  
+  ## data organization; these computations only need to be done once
+  Y.vec = as.vector(t(Y))
+  IIP = kronecker(diag(1, I, I), P.mat)
+  WIk = kronecker(Wi, diag(1, Kt, Kt))
+  tWIW = t(WIk) %*% IIP %*% WIk
+  tWI = t(WIk) %*% IIP
+  
+  
+  ## initial estimation and hyperparameter choice
+  vec.bz = solve(kronecker(t(Z.des)%*% Z.des, t(Theta) %*% Theta)) %*% t(kronecker(Z.des, Theta)) %*% Y.vec
+  bz = matrix(vec.bz, nrow = Kt, ncol = I)
+  
+  w.temp = kronecker(t(Wi), diag(1, Kt, Kt))
+  vec.bw = solve(tWIW) %*% tWI %*% vec.bz
+  bw = matrix(vec.bw, Kt, p)
+  
+  Yhat = as.matrix(Z.des %*% t(bz) %*% t(Theta))
+  varhat = var(as.vector(Y - Yhat))
+  
+  Psi = diag(varhat*IJ, D, D)
+  v = IJ
+  inv.sig = solve(Psi/v)
+  
+  Az = I*Kt / 2
+  Bz = sum(diag((t(bz) - Wi %*% t(bw)) %*% P.mat %*% t(t(bz) - Wi %*% t(bw))))
+  
+  Aw = Kt / 2
+  Bw = sapply(1:p, function(u) max(1, sum(diag( t(bw[,u]) %*% P.mat %*% (bw[,u])))))
+  
+  
+  ## matrices to store within-iteration estimates 
+  BW = array(NA, c(Kt, p, N.iter))
+  BW[,,1] = bw
+  BZ = array(NA, c(Kt, I, N.iter))
+  BZ[,,1] = bz
+  INV.SIG = array(NA, c(D, D, N.iter))
+  INV.SIG[,,1] = inv.sig 
+  LAMBDA.BW = matrix(NA, nrow = N.iter, ncol = p)
+  LAMBDA.BW[1,] = lambda.bw = Aw/Bw
+  LAMBDA.BZ = rep(NA, N.iter)
+  LAMBDA.BZ[1] = lambda.ranef = Az/Bz
+  
+  y.post = array(NA, dim = c(IJ, D, (N.iter - N.burn)))
+  
+  cat("Beginning Sampler \n")
+  pb <- txtProgressBar(min = 0, max = N.iter, initial = 0, style = 3)
+  for(i in 1:N.iter){
+    setTxtProgressBar(pb,i)
+    ###############################################################
+    ## update b-spline parameters for subject random effects
+    ###############################################################
+    
+    for(subj in 1:length(unique(SUBJ))){
+      
+      t.designmat.Z = t(kronecker(rep(1, Ji[subj]), Theta))
+      
+      sigma = solve(t.designmat.Z %*% kronecker(diag(1, Ji[subj], Ji[subj]), inv.sig) %*% t(t.designmat.Z) + 
+                      (lambda.ranef) * P.mat)
+      mu = sigma %*% (t.designmat.Z %*% kronecker(diag(1, Ji[subj], Ji[subj]), inv.sig) %*% (as.vector(t(Y[which(SUBJ == unique(SUBJ)[subj]),]))) +
+                        ((lambda.ranef) * P.mat) %*% bw %*% t(Wi[subj,]))
+      
+      bz[,subj] = matrix(mvrnorm(1, mu = mu, Sigma = sigma), nrow = Kt, ncol = 1)
+    }
+    ranef.cur = Z.des %*% t(bz) %*% t(Theta)
+    
+    ###############################################################
+    ## update b-spline parameters for fixed effects
+    ###############################################################
+    
+    sigma = solve( kronecker(diag(lambda.bw), P.mat) + ((lambda.ranef) * tWIW) )
+    mu = sigma %*% ((lambda.ranef) * tWI %*% as.vector(bz))
+    
+    bw = matrix(mvrnorm(1, mu = mu, Sigma = sigma), nrow = Kt, ncol = p)
+    
+    beta.cur = t(bw) %*% t(Theta)
+    
+    ###############################################################
+    ## update inverse covariance matrix
+    ###############################################################
+    
+    resid.cur = Y - ranef.cur
+    inv.sig = solve(riwish(v + IJ, Psi + t(resid.cur) %*% resid.cur))
+    
+    ###############################################################
+    ## update variance components
+    ###############################################################
+    
+    ## lambda for beta's
+    for(term in 1:p){
+      a.post = Aw + Kt/2
+      b.post = Bw[term] + 1/2 * bw[,term] %*% P.mat %*% bw[,term]
+      lambda.bw[term] = rgamma(1, a.post, b.post)
+    }
+    
+    ## lambda for random effects
+    a.post = Az + I*Kt/2
+    b.post = Bz + .5 * sum(sapply(1:I, function(u) (t(bz[,u]) - Wi[u,] %*% t(bw)) %*% P.mat %*% t(t(bz[,u]) - Wi[u,] %*% t(bw)) ))
+    lambda.ranef = rgamma(1, a.post, b.post)
+    
+    ###############################################################
+    ## save this iteration's parameters
+    ###############################################################
+    
+    BW[,,i] = as.matrix(bw)
+    BZ[,,i] = as.matrix(bz)
+    
+    INV.SIG[,,i] = inv.sig
+    LAMBDA.BW[i,] = lambda.bw
+    LAMBDA.BZ[i] = lambda.ranef
+    
+    if(i > N.burn){
+      y.post[,,i - N.burn] = ranef.cur
+    }
+    
+  }
+  close(pb)
+  ###############################################################
+  ## compute posteriors for this dataset
+  ###############################################################
+  
+  ## main effects
+  beta.post = array(NA, dim = c(p, D, (N.iter - N.burn)))
+  for(n in 1:(N.iter - N.burn)){
+    beta.post[,,n] = t(BW[,, n + N.burn]) %*% t(Theta)
+  }
+  
+  beta.pm = apply(beta.post, c(1,2), mean)
+  beta.LB = apply(beta.post, c(1,2), quantile, c(.025))
+  beta.UB = apply(beta.post, c(1,2), quantile, c(.975))
+  
+  ## random effects
+  b.pm = matrix(NA, nrow = I, ncol = D)
+  for(i in 1:I){
+    b.post = matrix(NA, nrow = (N.iter - N.burn), ncol = D)
+    for(n in 1:(N.iter - N.burn)){
+      b.post[n,] = BZ[,i, n + N.burn] %*% t(Theta)
+    }
+    b.pm[i,] = apply(b.post, 2, mean)
+  }
+  
+  ## covariance matrix
+  sig.pm = solve(apply(INV.SIG, c(1,2), mean))
+  
+  ## export fitted values
+  ranef.pm = Z.des %*% b.pm
+  Yhat = apply(y.post, c(1,2), mean)
+  
+  #MCMC diagnostics
+  #b_w matrix
+  b.w.mcmc <- data.frame(matrix(BW, nrow=dim(BW)[3], byrow=TRUE))
+  b.w.mcmc <- mcmc(tail(b.w.mcmc, n = (N.iter - N.burn)))
+  print(effectiveSize(b.w.mcmc)[which.min(effectiveSize(b.w.mcmc))])
+  
+  #b_z matrix
+  b.z.mcmc <- data.frame(matrix(BZ, nrow=dim(BZ)[3], byrow=TRUE))
+  b.z.mcmc <- mcmc(tail(b.z.mcmc, n = (N.iter - N.burn)))
+  print(effectiveSize(b.z.mcmc)[which.min(effectiveSize(b.z.mcmc))])
+  
+  #lambda bz terms
+  lambda.bzs <- mcmc(tail(cbind(LAMBDA.BZ), n = (N.iter - N.burn)))
+  print(effectiveSize(lambda.bzs))
+  
+  #lambda bw terms
+  lambda.bws <- mcmc(tail(cbind(LAMBDA.BW), n = (N.iter - N.burn)))
+  print(effectiveSize(lambda.bws))
+  
+  ret = list(beta.pm, beta.LB, beta.UB, ranef.pm, sig.pm, Yhat)
+  names(ret) = c("beta.pm", "beta.LB", "beta.UB", "ranef.pm", "sig.pm", "Yhat")
+  
+  ret
+}
 
+
+test2 <- goldsmiths.one.d.gibbs(Y = test1$obs, fixed_design.mat = test1$x_design,
+                                random_design.mat = test1$z_design, Kt=5)
 
 #Goldsmith's modified Gibbs sampler for 3 dimensions
 goldsmiths.three.d.gibbs <- function(Y, fixed_design.mat, random_design.mat, 
@@ -540,3 +766,6 @@ goldsmiths.three.d.gibbs <- function(Y, fixed_design.mat, random_design.mat,
   
   ret
 }
+
+
+#plot the posterior output of the Gibbs samplers
